1c6d61cb0a0363e26056f34d512dbcb57a3a940c
santhoshsat <santhoshsat002@gmail.com>
2023-03-25 09:12:23 +0530

    function and variable updated

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,25 @@
-// js   file 
+// javascript file
+var arr1=[1,2,3]
+var arr2=[0,1,2,3,4,5]
+
+var min_arr1
+var max_arr1
+var min_arr2
+var max_arr2
+
+var nestedarray=(arr1,arr2)=>{
+    //finding min value  and max value in array1
+    min_arr1=Math.min(...arr1)
+    max_arr1=Math.max(...arr1)
+
+    //finding min value  and max value in array2
+    min_arr2=Math.min(...arr2)
+    max_arr2=Math.max(...arr2)
+
+    //checking condition
+    isnested(min_arr1,min_arr2,max_arr1,max_arr2)
+}
+
+var isnested=(min__arr1,min__arr2,max__arr1,max__arr2)=>{
+    
+}
