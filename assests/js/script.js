3f3833a0a89600ae6b5561a5313655a73de72573
santhoshsat <santhoshsat002@gmail.com>
2023-03-25 09:17:21 +0530

    final commit

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,3 @@
-// javascript file
 var arr1=[1,2,3]
 var arr2=[0,1,2,3,4,5]
 
@@ -12,14 +11,27 @@ var nestedarray=(arr1,arr2)=>{
     min_arr1=Math.min(...arr1)
     max_arr1=Math.max(...arr1)
 
-    //finding min value  and max value in array2
+    //finding min value and max value in array2
     min_arr2=Math.min(...arr2)
     max_arr2=Math.max(...arr2)
 
-    //checking condition
+    //calling isnested fn()
     isnested(min_arr1,min_arr2,max_arr1,max_arr2)
 }
 
+//checking condition
 var isnested=(min__arr1,min__arr2,max__arr1,max__arr2)=>{
-    
+    if ((min__arr1 > min__arr2) && (max__arr1 < max__arr2)) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
 }
+
+//calling function
+nestedarray(arr1,arr2)
+
+nestedarray([1, 2, 3, 4], [0, 6])
+nestedarray([3, 1], [4, 0])
+nestedarray([9, 9, 8], [8, 9])
+nestedarray([1, 2, 3, 4], [2, 3])
